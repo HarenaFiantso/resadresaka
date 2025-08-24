62061cacf083dc950c14e599a2f93733f7f6ba46
Fiantso Harena <fiantsorav@gmail.com>
2025-08-24 19:08:19 +0300

    chore(server): update the typescipt compilation config

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import authRouter from './routes/auth.route';
 
 const app = express();
 const PORT = process.env.PORT ?? 8080;
+
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
