bcf2f4b9b7cdad488050c4a0feae8089c589e8e5
Fiantso Harena <fiantsorav@gmail.com>
2025-08-24 18:25:59 +0300

    chore: update .gitignore

--- apps/server/server.ts
+++ apps/server/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
